7512e7a1d097bb2a5dde7ebd494264cbf01f5755
mohit-sankhla2002 <100336545+mohit-sankhla2002@users.noreply.github.com>
2023-11-17 19:49:39 +0530

    init

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::net::{ TcpListener, TcpStream };
+use std::io::prelude::*;
+use std::fs;
+
+fn main() {
+    let listener = 
+        TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        // pass the incoming TcpStream to the handle connection to handle the connection 
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream:TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    let contents = fs::read_to_string("index.html").unwrap();
+
+    let response = format!(
+        "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        contents.len(),
+        contents
+    );
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
